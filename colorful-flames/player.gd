2e0d5a7d54a29a1b4e59e8879ed9f1d1ceda0d61
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2026-08-14 13:05:22 -0700

    Fix fire spread for both left and right directions

--- colorful-flames/player.gd
+++ colorful-flames/player.gd
@@ -12,17 +12,21 @@ const JUMP_VELOCITY = -300.0
 var flames_spreading: bool = false
 var coroutine_finished: bool = false
 var fire_spawn_init_position: Vector2
+var fire_spawn_origin: Vector2
+var fire_spread_direction: int
 var fsi: int
 
 @onready var fire_scene = preload("res://flames.tscn")
 @onready var jump_buffer_timer: Timer = $JumpBufferTimer
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 func _ready() -> void:
 	fire_spawn_init_position = $FireSpawnPosition.position
+	fire_spawn_origin = $FireSpawnPosition.position
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed(&"set_fire"):
 		flames_spreading = true
 		coroutine_finished = true
@@ -52,11 +56,14 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		#velocity.x = direction * SPEED
 		velocity.x = move_toward(velocity.x, direction * SPEED, accel)
-		#velocity.x = move_toward(velocity.x, direction * MAX_SPEED, accel)
+		sprite_2d.flip_h = true if direction < 0 else false
 	else:
 		velocity.x = move_toward(velocity.x, 0, accel)
+	
+	fire_spread_direction = -1 if sprite_2d.flip_h else 1
+	$FireSpawnPosition.position.x = -fire_spawn_init_position.x if sprite_2d.flip_h else fire_spawn_init_position.x
+	if $Timer.time_left == 0: fire_spawn_origin = $FireSpawnPosition.position
 
 	move_and_slide()
 
@@ -65,8 +72,8 @@ func spawn_flames():
 	coroutine_finished = false
 	
 	var flames = fire_scene.instantiate()
-	flames.position.x = fire_spawn_init_position.x + (fsi * fire_spread_amount)
-	flames.position.y = fire_spawn_init_position.y
+	flames.position.x = fire_spawn_origin.x + (fsi * fire_spread_amount * fire_spread_direction)
+	flames.position.y = fire_spawn_origin.y
 	add_child(flames)
 	var mother = get_parent()
 	# Defer call since new nodes cannot get added to parent until all child nodes have been accounted for
@@ -82,3 +89,5 @@ func spawn_flames():
 func _on_timer_timeout() -> void:
 	flames_spreading = false
 	coroutine_finished = false
+	fsi = 0
+	fire_spawn_origin = $FireSpawnPosition.position
