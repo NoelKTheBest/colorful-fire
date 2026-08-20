e5e11700a77d1d8eb2eee97c7c1ddcbc8837e735
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2026-08-20 15:53:35 -0700

    Change collision layers on flames, ground, and player and set fire to delete after timeout

--- colorful-flames/flames.gd
+++ colorful-flames/flames.gd
@@ -1,16 +1,9 @@
-extends AnimatedSprite2D
+extends RigidBody2D
 
-@export var max_speed = 100
-@export var acceleration = 20
-var speed : int
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#@export var max_speed = 100
+#@export var acceleration = 20
+#var speed : int
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	speed = move_toward(speed, max_speed, acceleration)
-	position.y += speed * delta
+func _on_timer_timeout() -> void:
+	queue_free()
